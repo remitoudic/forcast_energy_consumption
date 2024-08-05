43a61017e062a03b0e0de4be78b75fb6175e6337
Remus <remitouic@gmail.com>
2024-08-05 16:54:38 +0200

    Fix: lint

--- 3-deployment/app/main.py
+++ 3-deployment/app/main.py
@@ -5,10 +5,8 @@ from app.database import endpoints_db
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(
-    title="Forcast Energy Comsumption",
-    description= "Mlop course project"
-)
+app = FastAPI(title="Forcast Energy Comsumption",
+              description="Mlop course project")
 origins = ["*"]
 
 app.add_middleware(
